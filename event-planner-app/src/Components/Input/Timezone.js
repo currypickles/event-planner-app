afad676d71bd1cc9d000246305fef2f8a8a402d1
Davin <dvntaka@yahoo.com>
2020-05-05 16:15:39 -1000

    seens to work

--- event-planner-app/src/Components/Input/Timezone.js
+++ event-planner-app/src/Components/Input/Timezone.js
@@ -6,11 +6,11 @@ const timezone = (props)  => (
         <label>Timezone:
             <select name={props.name} onChange={props.select} defaultValue='Pacific/Honolulu' className='select-margin'>
                 <option value='Pacific/Honolulu'>Hawaii Standard Time</option>
-                <option value='America/Anchorage'>Alaska Standard Time - Anchorage</option>
-                <option value='America/Los_Angeles'>Pacific Standard Time - Los Angeles</option>
-                <option value='America/Denver'>Mountain Standard Time - Denver</option>
-                <option value='America/Chicago'>Central Standard Time - Chicago</option>
-                <option value='America/New_York'>Eastern Standard Time - New York</option>
+                <option value='America/Anchorage'>Alaska Time - Anchorage</option>
+                <option value='America/Los_Angeles'>Pacific Time - Los Angeles</option>
+                <option value='America/Denver'>Mountain Time - Denver</option>
+                <option value='America/Chicago'>Central Time - Chicago</option>
+                <option value='America/New_York'>Eastern Time - New York</option>
             </select>
         </label>
     </div>
